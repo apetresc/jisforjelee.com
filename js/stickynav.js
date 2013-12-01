c06b3fdeeead461087f425928bd5b405a2cefc5a
Adrian Petrescu <adrian.petrescu@maluuba.com>
2013-12-01 03:54:03 -0500

    Disable sticky nav if on mobile-sized screen

--- js/stickynav.js
+++ js/stickynav.js
@@ -1,5 +1,10 @@
 var resizeFloat = function() {
     $('#sidebar-float').width($('#sidebar').width());
+    if (window.innerWidth <= 640) {
+        $('#sidebar-float').addClass('floating-fixed-width-disabled');
+    } else {
+        $('#sidebar-float').removeClass('floating-fixed-width-disabled');
+    }
 }
 
 $(window).resize(resizeFloat);
